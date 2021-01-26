03db0e456f7212f3ef25b6577e6e24a283c916d9
AndreElmar94 <atir.lapinskaya@gmail.com>
2021-01-26 21:04:40 +0300

    add DeleteHandler.java

---
+++ src/main/java/handler/DeleteHandler.java
@@ -0,0 +1,28 @@
+package handler;
+import request.Request;
+public class DeleteHandler implements Handler {
+
+    @Override
+    public void handle(Request request) {
+        String payload = request.getPayload();
+
+        int count = 0;
+        count++;
+        for (int i = 0; i < payload.length(); i++) {
+            if (payload.charAt(i) == ' ') {
+                count++;
+            }
+            if (payload.charAt(i) == '/') {
+                count++;
+            }
+            if (payload.charAt(i) == ':') {
+                count++;
+            }
+            if (payload.charAt(i) == '.') {
+                count++;
+            }
+        }
+        System.out.println("result of deleteRequest`s handling (посчитать колисество слов в payload): " + payload + " = " + count);
+        System.out.println();
+    }
+}
