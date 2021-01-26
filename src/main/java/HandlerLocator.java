a8790a3b1fff65273cc1c6b5e5395c682bb8e4df
AndreElmar94 <atir.lapinskaya@gmail.com>
2021-01-26 21:06:18 +0300

    add HandlerLocator.java

---
+++ src/main/java/HandlerLocator.java
@@ -0,0 +1,17 @@
+import handler.CreateHandler;
+import handler.DeleteHandler;
+import handler.Handler;
+import handler.UpdateHandler;
+import request.RequestType;
+
+public class HandlerLocator {
+    public static Handler getHandler(RequestType type) {
+
+        switch (type) {
+            case CREATE: return new CreateHandler();
+            case DELETE: return new DeleteHandler();
+        }
+        return new UpdateHandler();
+    }
+
+}
