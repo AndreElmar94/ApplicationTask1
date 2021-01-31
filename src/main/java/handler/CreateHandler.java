22afcc2521103948f571d5a4bd1c373b2fbef334
AndreElmar94 <atir.lapinskaya@gmail.com>
2021-02-01 01:49:04 +0300

    update CreateHandler.java

--- src/main/java/handler/CreateHandler.java
+++ src/main/java/handler/CreateHandler.java
@@ -7,6 +7,7 @@ public class CreateHandler implements Handler {
     @Override
     public void handle(Request request) {
         String payload = request.getPayload();
+        System.out.println("createRequest payload: " + payload);
         String result = new StringBuffer(payload).reverse().toString();
         System.out.println("result of createRequest's handling (отобразить payload справа налево) : " + result);
         System.out.println();
