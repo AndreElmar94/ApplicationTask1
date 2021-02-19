e10eba3adabd238ce7a20873719d4cbb15a31576
AndreElmar94 <atir.lapinskaya@gmail.com>
2021-02-19 21:21:34 +0300

    UpdateHandler

--- src/main/java/handler/UpdateHandler.java
+++ src/main/java/handler/UpdateHandler.java
@@ -9,12 +9,9 @@ public class UpdateHandler implements Handler {
     @Override
     public String handle(Request request) {
         String payload = request.getPayload();
-        System.out.println("\n updateRequest payload: " + payload);
         Pattern pattern = Pattern.compile("(((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.){3}(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5]))$"); // [0-9] -> \\d
         Matcher matcher = pattern.matcher(payload);
         String result = String.valueOf(matcher.find());
-        System.out.print("result of updateRequest`s handling (проверить, я вляется ли payload валидным ip adress): ");
-        System.out.println(result);
         return result;
     }
 }
