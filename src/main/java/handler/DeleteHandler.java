3fdb8f3cc14e62732e42810b1ad68a0f44a90042
AndreElmar94 <atir.lapinskaya@gmail.com>
2021-03-17 19:43:23 +0300

    update DeleteHandler.java

--- src/main/java/handler/DeleteHandler.java
+++ src/main/java/handler/DeleteHandler.java
@@ -5,11 +5,6 @@ import request.Request;
 public class DeleteHandler implements Handler {
 
     @Override
-    public void payloadBeforeHandler(Request request) {
-        System.out.println(" payload: " + request.getPayload());
-    }
-
-    @Override
     public String handle(Request request) {
         String payload = request.getPayload();
         int count = payload.split("\s*[^A-z]\s*|[\s]+").length;
