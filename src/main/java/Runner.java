9ee7f2cbc1734ad198a1b75fd7d9c603118a158d
AndreElmar94 <atir.lapinskaya@gmail.com>
2021-03-17 18:54:20 +0300

    update Runner.java & update CreateHandler.java

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,52 +1,15 @@
+import handler.Files;
 import handler.Handler;
 import handler.HandlerLocator;
 import request.Request;
-import request.RequestType;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.List;
+import java.util.*;
 
 public class Runner {
     public static void main(String[] args) {
-
-        File fileForPayloadDelete = new File("src/main/resources/text/deleteRequest.txt");
-        File fileForPayloadUpdate = new File("src/main/resources/text/updateRequest.txt");
-        File fileForPayloadCreate = new File("src/main/resources/text/createRequest.txt");
-        FileReader fileReader;
-        BufferedReader bf;
-        List<Request> requests;
-        Request deleteRequest = null;
-        Request updateRequest = null;
-        Request createRequest = null;
-
-        List<File> files = List.of(fileForPayloadDelete, fileForPayloadUpdate, fileForPayloadCreate);
-        for (File file : files) {
-            try {
-                fileReader = new FileReader(file);
-                bf = new BufferedReader(fileReader);
-                String line;
-                while ((line = bf.readLine()) != null) {
-                    if (file.equals(fileForPayloadDelete)) {
-                        deleteRequest = new Request(line, RequestType.DELETE);
-                    }
-                    if (file.equals(fileForPayloadUpdate)) {
-                        updateRequest = new Request(line, RequestType.UPDATE);
-                    } else {
-                        createRequest = new Request(line, RequestType.CREATE);
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        requests = List.of(deleteRequest, updateRequest, createRequest);
+        Files files = new Files();
+        List<Request> requests = new LinkedList<>(files.workingWithFiles());
         HandlerLocator handlerLocator = new HandlerLocator();
-
         for (Request request : requests) {
             Handler suitableHandler = handlerLocator.getHandler(request.getType());
             suitableHandler.payloadBeforeHandler(request);
--- src/main/java/handler/CreateHandler.java
+++ src/main/java/handler/CreateHandler.java
@@ -5,11 +5,6 @@ import request.Request;
 public class CreateHandler implements Handler {
 
     @Override
-    public void payloadBeforeHandler(Request request) {
-        System.out.println(" payload: " + request.getPayload());
-    }
-
-    @Override
     public String handle(Request request) {
         String payload = request.getPayload();
         return new StringBuffer(payload).reverse().toString();
