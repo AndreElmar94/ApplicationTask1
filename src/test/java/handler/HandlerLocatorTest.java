21e8231ba175e1810c1bc6659d3301c3b2c78345
AndreElmar94 <atir.lapinskaya@gmail.com>
2021-03-17 19:47:37 +0300

    update HandlerLocatorTest.java

--- src/test/java/handler/HandlerLocatorTest.java
+++ src/test/java/handler/HandlerLocatorTest.java
@@ -1,26 +1,11 @@
-//package handler;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import request.RequestType;
-//
-//public class HandlerLocatorTest {
-//    HandlerLocator handlerLocator;
-//
-//    @BeforeEach
-//    public void initHandlerLocator() {
-//        handlerLocator = new HandlerLocator();
-//    }
-//
-//    @Test
-//    public void testGetHandler() {
-//        Handler actual1 = handlerLocator.getHandler(RequestType.DELETE);
-//        Handler actual2 = handlerLocator.getHandler(RequestType.UPDATE);
-//        Handler actual3 = handlerLocator.getHandler(RequestType.CREATE);
-//
-//        Assertions.assertEquals(new DeleteHandler(), actual1);
-//        Assertions.assertEquals(new UpdateHandler(), actual2);
-//        Assertions.assertEquals(new CreateHandler(), actual3);
-//    }
-//}
+package handler;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import request.RequestType;
+
+public class HandlerLocatorTest {
+    HandlerLocator handlerLocator;
+
+}
