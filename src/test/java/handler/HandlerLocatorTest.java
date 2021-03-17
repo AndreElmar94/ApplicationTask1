f4f1041887e88cd258b776dd0e2af7c693745796
AndreElmar94 <atir.lapinskaya@gmail.com>
2021-03-17 20:35:33 +0300

    update HandlerLocatorTest.java

--- src/test/java/handler/HandlerLocatorTest.java
+++ src/test/java/handler/HandlerLocatorTest.java
@@ -8,4 +8,19 @@ import request.RequestType;
 public class HandlerLocatorTest {
     HandlerLocator handlerLocator;
 
+    @BeforeEach
+    public void initHandlerLocator() {
+        handlerLocator = new HandlerLocator();
+    }
+
+    @Test
+    public void testGetHandler() {
+        Handler actualDelete = handlerLocator.getHandler(RequestType.DELETE);
+        Handler actualUpdate = handlerLocator.getHandler(RequestType.UPDATE);
+        Handler actualCreate = handlerLocator.getHandler(RequestType.CREATE);
+
+        Assertions.assertEquals(new DeleteHandler(), actualDelete);
+        Assertions.assertEquals(new UpdateHandler(), actualUpdate);
+        Assertions.assertEquals(new CreateHandler(), actualCreate);
+    }
 }
