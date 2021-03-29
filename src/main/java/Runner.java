dfba9be3c933b259f6144a45c0b8b53d86d134b6
AndreElmar94 <atir.lapinskaya@gmail.com>
2021-03-29 21:27:28 +0300

    update Runner.java

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,14 +1,15 @@
-import handler.Files;
+import workingWithFiles.FileNameFilter;
 import handler.Handler;
 import handler.HandlerLocator;
 import request.Request;
 
-import java.util.*;
+import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-        Files files = new Files();
-        List<Request> requests = new LinkedList<>(files.workingWithFiles());
+        FileNameFilter fileNameFilter = new FileNameFilter();
+        List<Request> requests = fileNameFilter.findRequestFromFiles("src/main/resources/text", ".txt");
+
         HandlerLocator handlerLocator = new HandlerLocator();
         for (Request request : requests) {
             Handler suitableHandler = handlerLocator.getHandler(request.getType());
